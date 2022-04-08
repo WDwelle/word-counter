dc175e01fa4a3ffe48e1925e3dd263619c7df167
WDwelle <willdwelle@frontier.com>
2022-04-08 13:07:01 -0700

    added line breaks

--- words.py
+++ words.py
@@ -8,7 +8,7 @@ counting = Counter(words) #counts each word
 listWords = counting.most_common() #Finds most common word
 
 
-print("Most Commonly used words in Text File is => ", counting.most_common()[0][0], "(", counting.most_common()[0][1], "times", ")"
-, counting.most_common()[1][0], "(", counting.most_common()[1][1], "times", ")",
-"and ", counting.most_common()[2][0], "(", counting.most_common()[2][1], "times", ")")
+print("Most Commonly used words in Text File: ","\n", counting.most_common()[0][0], "(", counting.most_common()[0][1], "times", ")"
+,"\n", counting.most_common()[1][0], "(", counting.most_common()[1][1], "times", ")",
+"\n", "and ", counting.most_common()[2][0], "(", counting.most_common()[2][1], "times", ")")
 #print("Frequency of most commonly used word in Text File is => ", counting.most_common()[0][1])
